5e86bce491199c319ec65d8be4736bedc9cb6ba7
rockkoca@gmail.com <end0405LESS>
2016-09-18 19:21:59 -0700

    andorid bug fix ZH-CN

--- server/note.js
+++ server/note.js
@@ -2,4 +2,5 @@
  * Created by k on 9/9/16.
  */
 // MONGO_URL=mongodb://localhost:27017/qidian CLUSTER_WORKERS_COUNT=auto meteor --port 3100 --settings settings.json
+// MONGO_URL=mongodb://localhost:27017/qidian CLUSTER_WORKERS_COUNT=2 meteor --port 3100 --settings settings.json
 // MONGO_URL=mongodb://localhost:27017/qidian  meteor --port 3100 --settings settings.json
