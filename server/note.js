7bdbdb00fb45d27252dc5f5434b08016e7f2ee5c
rockkoca@gmail.com <end0405LESS>
2016-09-18 21:59:02 -0700

    andorid bug fix ZH-CN

--- server/note.js
+++ server/note.js
@@ -4,3 +4,4 @@
 // MONGO_URL=mongodb://localhost:27017/qidian CLUSTER_WORKERS_COUNT=auto meteor --port 3100 --settings settings.json
 // MONGO_URL=mongodb://localhost:27017/qidian CLUSTER_WORKERS_COUNT=2 meteor --port 3100 --settings settings.json
 // MONGO_URL=mongodb://localhost:27017/qidian  meteor --port 3100 --settings settings.json
+// db.runCommand({ touch: "posts", data: true, index: true })
